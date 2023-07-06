ArtyomNelyub/guess-melody-9

6.2. Подключит redux к проекту.
Следующим шагом подключим `redux` к проекту.  Создадим новый модуль
`index.ts` в директории `store`. Нам потребуется функция `configureStore`
из пакета `@reduxjs/toolkit`. С её помощью мы инициализируем хранилище.

В файле `index.ts` (точка входа) воспользуемся `Provider` из пакета
`react-redux`. С помощью этого пакета мы настроим связь между React и
Redux. Обёрнём компонентом `Provider` компонент `App`.

project/src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import App from './components/app/app';
 import {questions} from './mocks/questions';
+import {store} from './store';
 
 const Setting = {
   ERRORS_COUNT: 3,
@@ -9,9 +11,11 @@ const Setting = {
 
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      errorsCount = {Setting.ERRORS_COUNT}
-      questions = {questions}
-    />
+    <Provider store = {store}>
+      <App
+        errorsCount = {Setting.ERRORS_COUNT}
+        questions = {questions}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));

cooperfirewalk/guess-melody-13

src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Provider} from 'react-redux';
 import App from './components/app/app';
 import {Setting} from './const';
 import {questions} from './mocks/questions';
+import {store} from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,9 +12,11 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App
-      errorsCount = {Setting.ErrorsCount}
-      questions = {questions}
-    />
+    <Provider store = {store}>
+      <App
+        errorsCount = {Setting.ErrorsCount}
+        questions = {questions}
+      />
+    </Provider>
   </React.StrictMode>,
 );